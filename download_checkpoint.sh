8ccd44d7da64d28a5d33dfdbf5cdf2e23315372d
RobbieLiuu <robbie0321132@gmail.com>
2025-05-05 15:29:22 +0800

    700

--- download_checkpoint.sh
+++ download_checkpoint.sh
@@ -4,3 +4,4 @@ gdown "https://drive.google.com/uc?id=1Jcn0ccgMUiZJuavYbe5ozz-6IJhc1SK9&export=d
 gdown "https://drive.google.com/uc?id=1ydqO7uReiyEtG58loFCy7d9YmNe2w8Vl&export=download"
 gdown "https://drive.google.com/uc?id=16kNmcAMrSQoLayIvEFoE2TiL5SXruWbe&export=download"
 gdown "https://drive.google.com/uc?id=1l6sW7eXgx09khpAbz6bV19fDwbLTO6U2&export=download"
+gdown "https://drive.google.com/uc?id=1zbA30UKCbq2sLinp0I1pYRhjVUsXnPqV&export=download"
