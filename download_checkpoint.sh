a6aa381584d99048b260db8f7cd63f9a57ade489
RobbieLiuu <robbie0321132@gmail.com>
2025-05-05 15:18:50 +0800

    1700

--- download_checkpoint.sh
+++ download_checkpoint.sh
@@ -3,3 +3,4 @@ pip install --upgrade gdown
 gdown "https://drive.google.com/uc?id=1Jcn0ccgMUiZJuavYbe5ozz-6IJhc1SK9&export=download"
 gdown "https://drive.google.com/uc?id=1ydqO7uReiyEtG58loFCy7d9YmNe2w8Vl&export=download"
 gdown "https://drive.google.com/uc?id=16kNmcAMrSQoLayIvEFoE2TiL5SXruWbe&export=download"
+gdown "https://drive.google.com/uc?id=1l6sW7eXgx09khpAbz6bV19fDwbLTO6U2&export=download"
